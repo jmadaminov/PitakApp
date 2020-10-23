4ffd25d316ef4e2952a798ba4c2c63511b4d9450
MJ <jahongir9779@gmail.com>
2020-10-23 10:26:26 +0500

    add car images logic start

--- app/src/main/java/com/badcompany/pitak/App.kt
+++ app/src/main/java/com/badcompany/pitak/App.kt
@@ -21,11 +21,6 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
  class App : Application() {
 
-//    lateinit var appComponent: AppComponent
-//    private var authComponent: AuthComponent? = null
-//    private var addCarComponent: AddCarComponent? = null
-//    private var addPostComponent: AddPostComponent? = null
-//    private var mainComponent: MainComponent? = null
 
 
     companion object {
@@ -60,65 +55,11 @@ import dagger.hilt.android.HiltAndroidApp
 
         OneSignal.idsAvailable { userId, registrationId ->
             Log.i("USERRR IDD ONE SIGNAL", "            $userId")
-            Log.i("USERRR IDD ONE SIGNAL", "            $userId")
-            Log.i("USERRR IDD ONE SIGNAL", "            $userId")
             uuid = userId
         }
 
-//        initAppComponent()
+
     }
-
-//    fun releaseAuthComponent() {
-//        authComponent = null
-//    }
-//
-//    fun authComponent(): AuthComponent {
-//        if (authComponent == null) {
-//            authComponent = appComponent.authComponent().create()
-//        }
-//        return authComponent as AuthComponent
-//    }
-//
-//    fun mainComponent(): MainComponent {
-//        if (mainComponent == null) {
-//            mainComponent = appComponent.mainComponent().create()
-//        }
-//        return mainComponent as MainComponent
-//    }
-//
-//    fun releaseMainComponent() {
-//        mainComponent == null
-//    }
-//
-//    fun initAppComponent() {
-//        appComponent = DaggerAppComponent.builder()
-//            .application(this)
-//            .build()
-//    }
-//
-//    fun releaseAddCarComponent() {
-//        addCarComponent = null
-//    }
-//
-//    fun releaseAddPostComponent() {
-//        addPostComponent = null
-//    }
-//
-//    fun addCarComponent(): AddCarComponent {
-//        if (addCarComponent == null) {
-//            addCarComponent = appComponent.addCarComponent().create()
-//        }
-//        return addCarComponent as AddCarComponent
-//    }
-//
-//
-//    fun addPostComponent(): AddPostComponent {
-//        if (addPostComponent == null) {
-//            addPostComponent = appComponent.addPostComponent().create()
-//        }
-//        return addPostComponent as AddPostComponent
-//    }
-
 
 }
 
