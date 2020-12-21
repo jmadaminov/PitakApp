75c9d5cf4bc92bb40a2e0f436f371de38a396ff2
MJ <jahongir9779@mail.ru>
2020-12-21 18:32:37 +0500

    add car images logic start

---
+++ app/src/main/java/com/badcompany/pitak/ui/edit_profile/EditProfileActivity.kt
@@ -0,0 +1,18 @@
+package com.badcompany.pitak.ui.edit_profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.badcompany.pitak.R
+import com.badcompany.pitak.ui.BaseActivity
+
+class EditProfileActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+
+
+
+}
---
+++ app/src/main/java/com/badcompany/pitak/ui/edit_profile/EditProfileViewModel.kt
@@ -0,0 +1,8 @@
+package com.badcompany.pitak.ui.edit_profile
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+
+class EditProfileViewModel @ViewModelInject constructor() : ViewModel() {
+}
